eb6db37d52c209e4f4955907f875b067131bc7ab
Kaibo Lu <lukaibolkb@gmail.com>
2019-04-01 10:46:15 -0700

    Initial Commit

---
+++ BBMetalImage/BBMetalImage.h
@@ -0,0 +1,19 @@
+//
+//  BBMetalImage.h
+//  BBMetalImage
+//
+//  Created by Kaibo Lu on 4/1/19.
+//  Copyright © 2019 Kaibo Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBMetalImage.
+FOUNDATION_EXPORT double BBMetalImageVersionNumber;
+
+//! Project version string for BBMetalImage.
+FOUNDATION_EXPORT const unsigned char BBMetalImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBMetalImage/PublicHeader.h>
+
+
