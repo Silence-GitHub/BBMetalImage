8d8d770578de35727a757931d7f88a8096a41521
Kaibo Lu <lukaibolkb@gmail.com>
2019-04-03 17:55:07 -0700

    Add notes

--- BBMetalImage/BBMetalImage/BBMetalWhiteBalanceFilter.swift
+++ BBMetalImage/BBMetalImage/BBMetalWhiteBalanceFilter.swift
@@ -16,8 +16,12 @@ private func convertTint(_ tint: Float) -> Float {
     return tint / 100
 }
 
+/// Adjusts the white balance of an image
 public class BBMetalWhiteBalanceFilter: BBMetalBaseFilter {
+    /// The temperature to adjust the image by, in ºK. A value of 4000 is very cool and 7000 very warm. The default value is 5000. Note that the scale between 4000 and 5000 is nearly as visually significant as that between 5000 and 7000.
     public var temperature: Float { didSet { _temperature = convertTemperature(temperature) } }
+    
+    /// The tint to adjust the image by. A value of -200 is very green and 200 is very pink. The default value is 0.
     public var tint: Float { didSet { _tint = convertTint(tint) } }
     
     private var _temperature: Float
