73afc646f903bb2de0b16f159abffafac60531b4
Kaibo Lu <lukaibolkb@gmail.com>
2019-04-10 17:21:28 -0700

    Add notes

--- BBMetalImage/BBMetalImage/BBMetalPolkaDotFilter.swift
+++ BBMetalImage/BBMetalImage/BBMetalPolkaDotFilter.swift
@@ -6,8 +6,11 @@
 //  Copyright © 2019 Kaibo Lu. All rights reserved.
 //
 
+/// Breaks an image up into colored dots within a regular grid
 public class BBMetalPolkaDotFilter: BBMetalBaseFilter {
+    /// How large the dots are, as a fraction of the width and height of the image (0.0 ~ 1.0, default 0.05)
     public var fractionalWidth: Float
+    /// What fraction of each grid space is taken up by a dot, from 0.0 to 1.0 with a default of 0.9
     public var dotScaling: Float
     
     public init(fractionalWidth: Float = 0.05, dotScaling: Float = 0.9) {
