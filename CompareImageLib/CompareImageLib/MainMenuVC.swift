178f96aadd0f679751df9bd5132056c7b156ba8a
Kaibo Lu <lukaibolkb@gmail.com>
2019-04-16 10:42:45 -0700

    Add CompareImageLib

---
+++ CompareImageLib/CompareImageLib/MainMenuVC.swift
@@ -0,0 +1,47 @@
+//
+//  MainMenuVC.swift
+//  CompareImageLib
+//
+//  Created by Kaibo Lu on 4/16/19.
+//  Copyright © 2019 Kaibo Lu. All rights reserved.
+//
+
+import UIKit
+
+typealias NoParamterBlock = () -> Void
+
+class MainMenuVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var list: [(String, NoParamterBlock)]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        list = []
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension MainMenuVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description(), for: indexPath)
+        cell.textLabel?.text = list[indexPath.row].0
+        return cell
+    }
+}
+
+extension MainMenuVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        list[indexPath.row].1()
+    }
+}
