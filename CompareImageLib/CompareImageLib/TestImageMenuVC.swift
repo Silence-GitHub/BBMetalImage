973b314c2b976c1c0db9c011649a5feabafdacc9
Kaibo Lu <lukaibolkb@gmail.com>
2019-04-16 11:40:45 -0700

    CompareImageLib adds TestImageVC

--- CompareImageLib/CompareImageLib/TestImageMenuVC.swift
+++ CompareImageLib/CompareImageLib/TestImageMenuVC.swift
@@ -1,5 +1,5 @@
 //
-//  MainMenuVC.swift
+//  TestImageMenuVC.swift
 //  CompareImageLib
 //
 //  Created by Kaibo Lu on 4/16/19.
@@ -8,29 +8,31 @@
 
 import UIKit
 
-typealias NoParamterBlock = () -> Void
+class TestImageMenuVC: UIViewController {
 
-class MainMenuVC: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    
     private var list: [(String, NoParamterBlock)]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let testImage = { [weak self] in
-            if let self = self { self.navigationController?.pushViewController(TestImageMenuVC(), animated: true) }
+        let testBB = { [weak self] in
+            if let self = self { self.navigationController?.pushViewController(TestImageVC(type: .BBWebImage), animated: true) }
         }
-        list = [("Test image", testImage)]
+        let testGPU = { [weak self] in
+            if let self = self { self.navigationController?.pushViewController(TestImageVC(type: .GPUImage), animated: true) }
+        }
+        list = [("BBMetalImage", testBB),
+                ("GPUImage", testGPU)]
         
+        let tableView = UITableView(frame: view.bounds)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
         tableView.dataSource = self
         tableView.delegate = self
+        view.addSubview(tableView)
     }
 }
 
-extension MainMenuVC: UITableViewDataSource {
+extension TestImageMenuVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count
     }
@@ -42,7 +44,7 @@ extension MainMenuVC: UITableViewDataSource {
     }
 }
 
-extension MainMenuVC: UITableViewDelegate {
+extension TestImageMenuVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         list[indexPath.row].1()
---
+++ CompareImageLib/CompareImageLib/TestImageVC.swift
@@ -0,0 +1,82 @@
+//
+//  TestImageVC.swift
+//  CompareImageLib
+//
+//  Created by Kaibo Lu on 4/16/19.
+//  Copyright © 2019 Kaibo Lu. All rights reserved.
+//
+
+import UIKit
+import BBMetalImage
+import GPUImage
+
+enum TestLib {
+    case BBWebImage
+    case GPUImage
+}
+
+class TestImageVC: UIViewController {
+
+    private let type: TestLib
+    private var image: UIImage!
+    private var imageView: UIImageView!
+    
+    init(type: TestLib) {
+        self.type = type
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        image = UIImage(named: "sunflower.jpg")
+        
+        title = "\(type)"
+        view.backgroundColor = .gray
+        
+        imageView = UIImageView(frame: CGRect(x: 10, y: 100, width: view.bounds.width - 20, height: view.bounds.height - 200))
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = image
+        view.addSubview(imageView)
+        
+        let button = UIButton(frame: CGRect(x: 10, y: view.bounds.height - 90, width: view.bounds.width - 20, height: 30))
+        button.backgroundColor = .blue
+        button.setTitle("Add filter", for: .normal)
+        button.setTitle("Remove filer", for: .selected)
+        button.addTarget(self, action: #selector(clickButton(_:)), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc private func clickButton(_ button: UIButton) {
+        button.isSelected = !button.isSelected
+        if button.isSelected {
+            let start = CACurrentMediaTime()
+            imageView.image = filteredImage
+            print("\(type) total time \(CACurrentMediaTime() - start)")
+        } else {
+            imageView.image = image
+        }
+    }
+    
+    private var filteredImage: UIImage? {
+        switch type {
+        case .BBWebImage:
+            return image.bb_lookupFiltered(withLookupTable: UIImage(named: "test_lookup")!.bb_metalTexture!)
+        case .GPUImage:
+            let lookupImageSource = GPUImagePicture(image: UIImage(named: "test_lookup")!)!
+            let lookupFilter = GPUImageLookupFilter()
+            lookupImageSource.addTarget(lookupFilter, atTextureLocation: 1)
+            lookupImageSource.processImage()
+            let imageSource = GPUImagePicture(image: image)!
+            imageSource.addTarget(lookupFilter)
+            lookupFilter.useNextFrameForImageCapture()
+            imageSource.processImage()
+            return lookupFilter.imageFromCurrentFramebuffer()
+        }
+    }
+}
