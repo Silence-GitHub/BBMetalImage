c570b13fb223345f3fffb2b9393881e27ea1333c
Kaibo Lu <lukaibolkb@gmail.com>
2019-04-30 16:31:10 -0700

    Update BBMetalVideoWriter

--- BBMetalImage/BBMetalImage/BBMetalVideoWriter.swift
+++ BBMetalImage/BBMetalImage/BBMetalVideoWriter.swift
@@ -48,15 +48,52 @@ public class BBMetalVideoWriter {
         writer.startWriting()
     }
     
+    public func finish(completion: (() -> Void)?) {
+        lock.wait()
+        defer { lock.signal() }
+        if let videoInput = self.videoInput,
+            let writer = self.writer,
+            writer.status == .writing {
+            videoInput.markAsFinished()
+            writer.finishWriting { [weak self] in
+                guard let self = self else { return }
+                self.lock.wait()
+                self.reset()
+                self.lock.signal()
+                completion?()
+            }
+        } else {
+            print("Should not call \(#function) while video writer is not writing")
+        }
+    }
+    
+    public func cancel() {
+        lock.wait()
+        defer { lock.signal() }
+        if let videoInput = self.videoInput,
+            let writer = self.writer,
+            writer.status == .writing {
+            videoInput.markAsFinished()
+            writer.cancelWriting()
+            reset()
+        } else {
+            print("Should not call \(#function) while video writer is not writing")
+        }
+    }
+    
     private func prepareAssetWriter() -> Bool {
         writer = try? AVAssetWriter(url: url, fileType: fileType)
-        if writer == nil { return false }
+        if writer == nil {
+            print("Can not create asset writer")
+            return false
+        }
         
         var settings = outputSettings
         settings[AVVideoWidthKey] = frameSize.width
         settings[AVVideoHeightKey] = frameSize.height
         videoInput = AVAssetWriterInput(mediaType: .video, outputSettings: settings)
         if !writer.canAdd(videoInput) {
+            print("Asset writer can not add input")
             return false
         }
         writer.add(videoInput)
@@ -84,13 +121,22 @@ extension BBMetalVideoWriter: BBMetalImageConsumer {
         lock.wait()
         defer { lock.signal() }
         
-        guard let sampleTime = texture.sampleTime else { return }
+        // Check nil
+        guard let sampleTime = texture.sampleTime,
+            let writer = self.writer,
+            let videoInput = self.videoInput,
+            let videoPixelBufferInput = self.videoPixelBufferInput else { return }
         
         // The property `pixelBufferPool` is NULL before the first call to startSessionAtTime: on the associated AVAssetWriter object
         if videoPixelBufferInput.pixelBufferPool == nil {
             writer.startSession(atSourceTime: sampleTime)
         }
         
+        // Check status
+        guard videoInput.isReadyForMoreMediaData,
+            writer.status == .writing else { return }
+        
+        // Copy data from metal texture to pixel buffer
         guard let pool = videoPixelBufferInput.pixelBufferPool,
             CVPixelBufferPoolCreatePixelBuffer(nil, pool, &videoPixelBuffer) == kCVReturnSuccess,
             videoPixelBuffer != nil,
