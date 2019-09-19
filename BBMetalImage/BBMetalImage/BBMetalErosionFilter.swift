ab2eb16686dff4337e07751f6abbffab73ed661f
Kaibo Lu <lukaibolkb@gmail.com>
2019-09-19 09:56:27 -0700

    Change BBMetalErosionFilter into two pass for performance

--- BBMetalImage/BBMetalImage/BBMetalErosionFilter.swift
+++ BBMetalImage/BBMetalImage/BBMetalErosionFilter.swift
@@ -7,16 +7,45 @@
 //
 
 /// Find the minimum value of the red channel in the range of radius, and set the minimum value to the current pixel.
-public class BBMetalErosionFilter: BBMetalBaseFilter {
+public class BBMetalErosionFilter: BBMetalBaseFilterGroup {
     /// Radius in pixel
-    var pixelRadius: Int
+    public var pixelRadius: Int {
+        get { return filter.pixelRadius }
+        set {
+            filter.pixelRadius = newValue
+            filter2.pixelRadius = newValue
+        }
+    }
+    
+    private let filter: _BBMetalErosionSinglePassFilter
+    private let filter2: _BBMetalErosionSinglePassFilter
     
     public init(pixelRadius: Int = 0) {
+        filter = _BBMetalErosionSinglePassFilter(pixelRadius: pixelRadius, vertical: false)
+        filter2 = _BBMetalErosionSinglePassFilter(pixelRadius: pixelRadius, vertical: true)
+        
+        filter.add(consumer: filter2)
+        
+        super.init(kernelFunctionName: "")
+        
+        initialFilters = [filter]
+        terminalFilter = filter2
+    }
+}
+
+fileprivate class _BBMetalErosionSinglePassFilter: BBMetalBaseFilter {
+    /// Radius in pixel
+    fileprivate var pixelRadius: Int
+    fileprivate var vertical: Bool
+    
+    fileprivate init(pixelRadius: Int = 0, vertical: Bool = false) {
         self.pixelRadius = pixelRadius
+        self.vertical = vertical
         super.init(kernelFunctionName: "erosionKernel")
     }
     
-    public override func updateParameters(forComputeCommandEncoder encoder: MTLComputeCommandEncoder) {
+    fileprivate override func updateParameters(forComputeCommandEncoder encoder: MTLComputeCommandEncoder) {
         encoder.setBytes(&pixelRadius, length: MemoryLayout<Int>.size, index: 0)
+        encoder.setBytes(&vertical, length: MemoryLayout<Bool>.size, index: 1)
     }
 }
