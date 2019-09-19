abb94a2782ce8ed04009070c605cb8e7fe07c982
Kaibo Lu <lukaibolkb@gmail.com>
2019-09-19 10:16:53 -0700

    Change BBMetalDilationFilter into two pass for performance

--- BBMetalImage/BBMetalImage/BBMetalDilationFilter.swift
+++ BBMetalImage/BBMetalImage/BBMetalDilationFilter.swift
@@ -7,16 +7,45 @@
 //
 
 /// Find the maximum value of the red channel in the range of radius, and set the maximum value to the current pixel.
-public class BBMetalDilationFilter: BBMetalBaseFilter {
+public class BBMetalDilationFilter: BBMetalBaseFilterGroup {
     /// Radius in pixel
-    var pixelRadius: Int
+    public var pixelRadius: Int {
+        get { return filter.pixelRadius }
+        set {
+            filter.pixelRadius = newValue
+            filter2.pixelRadius = newValue
+        }
+    }
+    
+    private let filter: _BBMetalDilationSinglePassFilter
+    private let filter2: _BBMetalDilationSinglePassFilter
     
     public init(pixelRadius: Int = 0) {
+        filter = _BBMetalDilationSinglePassFilter(pixelRadius: pixelRadius, vertical: false)
+        filter2 = _BBMetalDilationSinglePassFilter(pixelRadius: pixelRadius, vertical: true)
+        
+        filter.add(consumer: filter2)
+        
+        super.init(kernelFunctionName: "")
+        
+        initialFilters = [filter]
+        terminalFilter = filter2
+    }
+}
+
+fileprivate class _BBMetalDilationSinglePassFilter: BBMetalBaseFilter {
+    /// Radius in pixel
+    fileprivate var pixelRadius: Int
+    fileprivate var vertical: Bool
+    
+    fileprivate init(pixelRadius: Int = 0, vertical: Bool = false) {
         self.pixelRadius = pixelRadius
+        self.vertical = vertical
         super.init(kernelFunctionName: "dilationKernel")
     }
     
-    public override func updateParameters(forComputeCommandEncoder encoder: MTLComputeCommandEncoder) {
+    fileprivate override func updateParameters(forComputeCommandEncoder encoder: MTLComputeCommandEncoder) {
         encoder.setBytes(&pixelRadius, length: MemoryLayout<Int>.size, index: 0)
+        encoder.setBytes(&vertical, length: MemoryLayout<Bool>.size, index: 1)
     }
 }
