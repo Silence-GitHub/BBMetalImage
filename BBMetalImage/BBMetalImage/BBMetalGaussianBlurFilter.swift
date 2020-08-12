a91481c4a81a5d7b9c51363d51eb138c78d290ff
Kaibo Lu <lukaibolkb@gmail.com>
2020-08-12 10:30:17 -0700

    BBMetalGaussianBlurFilter can set sigma value

--- BBMetalImage/BBMetalImage/BBMetalGaussianBlurFilter.swift
+++ BBMetalImage/BBMetalImage/BBMetalGaussianBlurFilter.swift
@@ -11,13 +11,16 @@ import MetalPerformanceShaders
 /// A filter that convolves an image with a Gaussian blur of a given sigma in both the x and y directions
 public class BBMetalGaussianBlurFilter: BBMetalBaseFilter {
     /// The standard deviation of the gaussian blur filter
-    public let sigma: Float
+    public var sigma: Float { didSet { _kernel = nil } }
     
-    private lazy var kernel: MPSImageGaussianBlur = {
+    private var kernel: MPSImageGaussianBlur {
+        if let k = _kernel { return k }
         let k = MPSImageGaussianBlur(device: BBMetalDevice.sharedDevice, sigma: sigma)
         k.edgeMode = .clamp
+        _kernel = k
         return k
-    }()
+    }
+    private var _kernel: MPSImageGaussianBlur!
     
     public init(sigma: Float) {
         self.sigma = sigma
